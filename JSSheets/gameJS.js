db5d8a9535b7e478d1a6350d2f10b116c0e58049
Zion Chen <CPProjectGame>
2020-02-11 18:58:26 +0800

    Rounded Numbers

--- JSSheets/gameJS.js
+++ JSSheets/gameJS.js
@@ -2,66 +2,65 @@ var energy = 0;
 var energyPerClick = 1;
 var pollutionStart = false;
 var pollution = 0;
-var pollutionPerSec = 1;
+var roundedOffPollution = Math.round(pollution);
+var pollutionPerSec = 0.33333333333333333333333333;
 var cp1=false;
 var cp2=false;
 var cp3=false;
-var array = [0, 1, true]
+var mainGameLoop;
 
 //init
 document.getElementById("energyConverted").innerHTML = energy;
-document.getElementById("pollutionEarned").innerHTML = pollution;
+document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
 
 document.getElementById("hire").onclick = convertEnergyFoo;
 document.getElementById("cp1").onclick = lessenEnergy1;
 document.getElementById("cp2").onclick = lessenEnergy2;
 document.getElementById("cp3").onclick = lessenEnergy3;
 document.getElementById("up").onclick = upgrade;
-
+window.onload = startLoop();
 
 function convertEnergyFoo() {
   energy = energy+energyPerClick;
   document.getElementById("energyConverted").innerHTML = energy;
   document.getElementById("hire").style.backgroundColor = "#00FF00";
   setTimeout(function(){document.getElementById("hire").style.backgroundColor = "#00008B";}, 100);
-  if (energy >= 50) {
-    pollutionStart = true;
-    startLoop();
-  }
 }
 
 function startLoop() {
   //var mainGameLoop=setInterval(foo, 1000, [pollution, pollutionPerSec, pollutionStart]);
   //var mainGameLoop=setInterval(foo.bind(null, [pollution, pollutionPerSec, pollutionStart]), 1000);
-  var mainGameLoop2=setInterval(function foo2() {[pollution, pollutionPerSec, pollutionStart] =
-    foo([pollution, pollutionPerSec, pollutionStart]);}, 1000);
-
+  if (energy >= 50) {
+    pollutionStart = true;
+  }
+  mainGameLoop=setInterval(function foo2() {[pollution, pollutionPerSec, pollutionStart, roundedOffPollution, mainGameLoop] =
+    foo([pollution, pollutionPerSec, pollutionStart, roundedOffPollution, mainGameLoop]);}, 1000);
 }
 
-function foo([pollution, pollutionPerSec, pollutionStart]) {
-  if (pollutionStart = true) {
+function foo([pollution, pollutionPerSec, pollutionStart, roundedOffPollution, mainGameLoop]) {
+  console.log(pollutionStart);
+  if (pollutionStart == true) {
     pollution = pollution + pollutionPerSec;
-    document.getElementById("pollutionEarned").innerHTML = pollution;
+    console.log("pollution is: " + pollution);
+
+    roundedOffPollution = Math.round(pollution);
+    document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
 
     //lose
     if (pollution >= 100) {
       alert("You Lose!");
+      energy = 0;
+      energyPerClick = 1;
+      pollutionStart = false;
+      pollution = 0;
+      roundedOffPollution = Math.round(pollution);
+      pollutionPerSec = 0.33333333333333333333333333;
+      cp1=false;
+      cp2=false;
+      cp3=false;
       clearInterval(mainGameLoop);
-      var energy = 0;
-      var energyPerClick = 1;
-      var pollutionStart = false;
-      var pollution = 0;
-      var pollutionPerSec=1;
-      var cp1=false;
-      var cp2=false;
-      var cp3=false;
       document.getElementById("energyConverted").innerHTML = energy;
-      document.getElementById("pollutionEarned").innerHTML = pollution;
-      convertEnergyFoo();
-      upgrade();
-      lessenEnergy1();
-      lessenEnergy2();
-      lessenEnergy3();
+      document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
     }
 
     //win
@@ -70,7 +69,8 @@ function foo([pollution, pollutionPerSec, pollutionStart]) {
       document.getElementById("win").onclick = winGame;
     }
 
-    return [pollution, pollutionPerSec, pollutionStart]
+    return [pollution, pollutionPerSec, pollutionStart,roundedOffPollution, mainGameLoop]
+
   }
 }
 
@@ -78,15 +78,18 @@ function foo([pollution, pollutionPerSec, pollutionStart]) {
 //win
 function winGame() {
   alert("You Win!");
-  var energy = 0;
-  var energyPerClick = 1;
-  var pollutionStart = false;
-  var pollution = 0;
-  var pollutionPerSec=1;
+  energy = 0;
+  energyPerClick = 1;
+  pollutionStart = false;
+  pollution = 0;
+  roundedOffPollution = Math.round(pollution);
+  pollutionPerSec = 0.33333333333333333333333333;
+  cp1=false;
+  cp2=false;
+  cp3=false;
   document.getElementById("energyConverted").innerHTML = energy;
-  document.getElementById("pollutionEarned").innerHTML = pollution;
+  document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
   document.getElementById("win").style.visibility = "hidden";
-  document.getElementById("wincounter").innerHTML = winCount;
   clearInterval(mainGameLoop);
 }
 
@@ -118,7 +121,7 @@ function lessenEnergy1() {
     setTimeout(function(){document.getElementById("cp1").style.backgroundColor = "#00008B";}, 100);
     document.getElementById("cp1").style.backgroundColor = "#00FF00";
     pollutionPerSec=((pollutionPerSec*2)*0.7)/2;
-    document.getElementById("pollutionEarned").innerHTML = pollution;
+    document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
   } else {
     document.getElementById("cp1").style.backgroundColor = "#FF0000";
     setTimeout(function(){document.getElementById("cp1").style.backgroundColor = "#00008B";}, 100);
@@ -132,7 +135,7 @@ function lessenEnergy2() {
     setTimeout(function(){document.getElementById("cp2").style.backgroundColor = "#00008B";}, 100);
     document.getElementById("cp2").style.backgroundColor = "#00FF00";
     pollutionPerSec=((pollutionPerSec*2)*0.5)/2;
-    document.getElementById("pollutionEarned").innerHTML = pollution;
+    document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
   } else {
     document.getElementById("cp2").style.backgroundColor = "#FF0000";
     setTimeout(function(){document.getElementById("cp2").style.backgroundColor = "#00008B";}, 100);
@@ -147,7 +150,7 @@ function lessenEnergy3() {
     document.getElementById("cp3").style.backgroundColor = "#00FF00";
     setTimeout(function foo() {pollution = 0; energy = 0;}, 10000);
     pollutionStart=true;
-    document.getElementById("pollutionEarned").innerHTML = pollution;
+    document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
   } else {
     document.getElementById("cp3").style.backgroundColor = "#FF0000";
     setTimeout(function(){document.getElementById("cp3").style.backgroundColor = "#00008B";}, 100);
@@ -161,7 +164,7 @@ function upgrade() {
     setTimeout(function(){document.getElementById("up").style.backgroundColor = "#00008B";}, 100);
     document.getElementById("up").style.backgroundColor = "#00FF00";
     energyPerClick=energyPerClick+1;
-    document.getElementById("pollutionEarned").innerHTML = pollution;
+    document.getElementById("pollutionEarned").innerHTML = roundedOffPollution;
   } else {
     document.getElementById("up").style.backgroundColor = "#FF0000";
     setTimeout(function(){document.getElementById("up").style.backgroundColor = "#00008B";}, 100);
