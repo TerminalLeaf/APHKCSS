45df20585dee0c00c6042cd72b8d617186a7222a
Zion Chen <CPProjectGame>
2020-02-13 08:47:21 +0800

    Added a few Style edits, and made the website look better

--- JSSheets/intropagemain.js
+++ JSSheets/intropagemain.js
@@ -1,11 +1,43 @@
-var imgCounter = 1;
+var imgCounter = 2;
 
 document.addEventListener('DOMContentLoaded', (event)=> {setInterval(function foo(){
     if (imgCounter == 1){
-      setTimeout(function(){console.log("hi!"); document.getElementById("img").src = "Images/IntroIMG.png"; imgCounter=imgCounter+1;}, 3000);
+      setTimeout(function foo(){
+        document.getElementById("smallsolIMG").style.top = "0px";
+        document.getElementById("smallsolIMG").style.borderTop = "none";
+        document.getElementById("smallgmeIMG").style.top = "0px";
+        document.getElementById("smallgmeIMG").style.borderTop = "none";
+        document.getElementById("smallintIMG").style = "position: relative;border-top: 5px solid darkred;top: -1px;"
+        document.getElementById("img").src = "Images/IntroIMG.png";
+        document.getElementById("urlForPage").href = "index.html";
+        imgCounter=imgCounter+1;
+      }, 3000);
     } else if (imgCounter == 2){
-      setTimeout(function(){console.log("hi!"); document.getElementById("img").src = "Images/GameIMG.png"; imgCounter=imgCounter+1;}, 3000);
+      setTimeout(function foo(){
+        document.getElementById("smallsolIMG").style.top = "0px";
+        document.getElementById("smallsolIMG").style.borderTop = "none";
+        document.getElementById("smallintIMG").style.top = "0px";
+        document.getElementById("smallintIMG").style.borderTop = "none";
+        document.getElementById("smallgmeIMG").style = "position: relative;border-top: 5px solid darkred;top: -1px;"
+        document.getElementById("img").src = "Images/GameIMG.png";
+        document.getElementById("urlForPage").href = "gamePage.html";
+        imgCounter=imgCounter+1;
+      }, 3000);
     } else if (imgCounter == 3){
-      setTimeout(function(){console.log("hi!"); document.getElementById("img").src = "Images/SolIMG.png"; imgCounter=1;}, 3000);
+      setTimeout(function foo(){
+        document.getElementById("smallgmeIMG").style.top = "0px";
+        document.getElementById("smallgmeIMG").style.borderTop = "none";
+        document.getElementById("smallintIMG").style.top = "0px";
+        document.getElementById("smallintIMG").style.borderTop = "none";
+        document.getElementById("smallsolIMG").style = "position: relative;border-top: 5px solid darkred;top: -1px;"
+        document.getElementById("img").src = "Images/SolIMG.png";
+        document.getElementById("urlForPage").href = "solutionsPage.html";
+        imgCounter=1;
+      }, 3000);
     }
   }, 3000);});
+
+
+/*position: relative;
+border-top: 5px solid darkred;
+top: -1px;*/
